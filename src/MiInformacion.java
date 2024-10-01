1933267d76693ef8f372e83d309d329310e78711
Natalia <nataliacruzleiva@gmail.com>
2024-10-01 16:59:20 -0400

    Primera versión del proyecto MiInformacion

---
+++ src/MiInformacion.java
@@ -0,0 +1,9 @@
+public class MiInformacion {
+    public static void main(String[] args) {
+        // Imprimir la información personal
+        System.out.println("Hola, mi nombre es Natalia Cruz.");
+        System.out.println("Tengo 28 años de edad.");
+        System.out.println("Soy de Paraguay y vivo en la ciudad de Luque.");
+        System.out.println("Me gusta mucho ver series, videojuegos y andar en rollers.");
+    }
+}
